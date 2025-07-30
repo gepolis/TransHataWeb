de305b741e6a2431cf9d607b4c87fc8c62680397
NAVI <ivanaksenov2010@mail.ru>
2025-07-30 22:07:10 +0300

    upd

--- app.py
+++ app.py
@@ -7,7 +7,12 @@ app.secret_key = 'GU_GEPOLIS_GUAPPSUPPORT_ADMIN_SECRET_KEY_2'
 def hello_world():  # put application's code here
     return render_template("index.html")
 
-
+@app.route('/new')
+def new():  # put application's code here
+    return render_template("new.html")
+@app.route('/purple')
+def purple():  # put application's code here
+    return render_template("purple.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
